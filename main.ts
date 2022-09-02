e04f42348f1e7cdf20652c37c647c945d2f1027d
francoxd12 <112702577+francoxd12@users.noreply.github.com>
2022-09-02 09:01:55 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(randint(1, 10))
+    basic.showString("AMOR")
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showString("...?")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Hola")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("UwU")
+})
+basic.forever(function () {
+	
+})
